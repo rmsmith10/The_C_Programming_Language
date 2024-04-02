541e711d133602e8ac74a39b190608f07c4d84d9
Reginal Smith <rmsmith111@gmail.com>
2024-04-03 01:43:24 +0900

    Add files via upload

---
+++  nested control.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+/* Line generator limited to 20 lines */
+int main()
+{
+    int line_number = 0;
+    
+    scanf("%d", &line_number);
+    do{
+	for(int i = 0; i < line_number; ++i){
+        printf("\n");
+        if(line_number == 0)
+            printf("*#");
+        else if(line_number > 20)
+            line_number = 20;
+        else{
+	    for(int j = 0; j <= i; ++j)
+	        printf("*#");
+        }
+	}
+	return 0;
+    }while(line_number <= 20);
+}
