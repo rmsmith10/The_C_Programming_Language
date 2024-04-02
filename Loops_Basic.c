707b09960e74b493de555fd9991d93f104de7a46
Reginal Smith <rmsmith111@gmail.com>
2024-04-03 01:05:28 +0900

    Add files via upload

---
+++ Loops_Basic.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int number_one = 0;
+    int number_two = 0;
+    int display_number = 0;
+	do{
+	    printf("Enter 1st Number: \n");
+	    scanf("%d", &number_one);
+	    if(!number_one)
+	        continue;
+	    printf("Enter 2nd Number: \n");
+	    scanf("%d", &number_two);
+	    if(number_one == 99 && number_two == 0){
+            printf("Sum: %d\nFinish.", number_one + number_two);
+            break;
+	    }
+	    else{
+	        ++display_number;
+	        printf("Sum: %d\n", number_one + number_two);
+	    }
+	}while(number_one != 0);
+	        
+	
+	return 0;
+}
